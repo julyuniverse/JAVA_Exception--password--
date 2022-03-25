5105cdbb58d682f39ad7e003ae4730341f21be4a
Lee Taesung <leetsxpro@gmail.com>
2022-03-25 22:30:45 +0900

    2022-03-25 Update3

--- src/PasswordException.java
+++ src/PasswordException.java
@@ -1,4 +1,4 @@
-public class PasswordException extends Exception {
+public class PasswordException extends Exception { // PasswordException 클래스는 프로그래머가 직접 만든 예외 처리. 즉, 사용자 정의 예외 처리 클래스.
     // 생성자
     public PasswordException(String message) {
         super(message);
